91c0ea7beda598fbf7fd6cea782e5b62eed60020
gawde-abhijeet <abhijeet.gawde@accenture.com>
2017-12-04 22:07:48 +0530

    using zipking instrumentation library for tracing purpose

--- source-app/front-end/server.js
+++ source-app/front-end/server.js
@@ -13,8 +13,32 @@ var request      = require("request")
   , orders       = require("./api/orders")
   , user         = require("./api/user")
   , metrics      = require("./api/metrics")
+  , CLSContext = require('zipkin-context-cls')
+  , {Tracer} = require('zipkin')
+  , {recorder} = require('./recorder')
+  , ctxImpl = new CLSContext('zipkin')
+  , localServiceName = 'frontend'
+  , tracer = new Tracer({ctxImpl, recorder, localServiceName})
   , app          = express()
 
+
+// instrument the server
+const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware;
+app.use(zipkinMiddleware({tracer}));
+
+// instrument the client
+const {restInterceptor} = require('zipkin-instrumentation-cujojs-rest');
+const zipkinRest = rest.wrap(restInterceptor, {tracer});
+
+// Allow cross-origin, traced requests. See http://enable-cors.org/server_expressjs.html
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', [
+    'Origin', 'Accept', 'X-Requested-With', 'X-B3-TraceId',
+    'X-B3-ParentSpanId', 'X-B3-SpanId', 'X-B3-Sampled'
+  ].join(', '));
+  next();
+});
 
 app.use(helpers.rewriteSlash);
 app.use(metrics);
